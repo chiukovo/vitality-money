e63904a68e7b8f2cc3db16359e092e3f8de66127
Quni <quniwork@gmail.com>
2019-11-01 15:48:28 +0800

    quni:20191101v1

--- components/UserInfo.vue
+++ components/UserInfo.vue
@@ -3,9 +3,14 @@
   .userInfo
     .userInfo-header {{ userInfo.Account }}(正常收單)
     .userInfo-content
-      el-table.table(:data='tableData', :show-header='false' style='width: 100%' border)
+      el-table.table(
+        :data='tableData'
+        :show-header='false'
+        :cell-class-name='tableCellClassName',
+        style='width: 100%'
+        border)
         el-table-column(prop='title', label='標題')
-        el-table-column(prop='infor', label='資訊')
+        el-table-column(prop='infor',label='資訊')
           //- 今日餘額需加入class .text-primary
 </template>
 <script>
@@ -39,9 +44,25 @@ export default {
           title: '今日餘額:',
           infor: userInfo.Money,
         }, {
-          title: '今日損益::',
+          title: '今日損益:',
           infor: userInfo.TodayMoney,
         }]
+    }
+  },
+  methods: {
+    tableCellClassName({ row, column, columnIndex }) {
+      if(row.title == '今日餘額:') {
+        if(columnIndex == 1) {
+          return 'text-infor'
+        }
+      }
+      if(row.title == '今日損益:') {
+        if(columnIndex == 1) {
+          if(row.infor < 0) {
+            return 'text-danger'
+          }
+        }
+      }
     }
   }
 }
