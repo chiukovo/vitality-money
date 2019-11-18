302f51064149ea873f070966a508fa9d2cf08e58
chiuko <chiuko@celloes.com>
2019-11-18 12:05:06 +0800

    fix

--- components/Footer.vue
+++ components/Footer.vue
@@ -8,6 +8,34 @@
 				a.news__item(href='#') 新增F G 版面,供黑馬元氣會員下單,B版供RSI DT 會員下單
 				a.news__item(href='#') 此版為測試帳號,損益不列入計算,此帳號將不定時刪除交易記錄與強平所有倉位!
 		#today
-			.today__date 2019年10月24日11時57分32秒
+			.today__date {{ dateTime }}
 				el-button(size='mini') 12-伺服器
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      dateTime: ''
+    }
+  },
+  mounted() {
+  	this.showDateTime()
+  },
+  methods: {
+  	showDateTime() {
+  		let now=new Date()//生成日期物件(完整的日期資訊)
+  		let y=now.getFullYear()//年份
+  		let M=now.getMonth()+1//月份
+  		let d=now.getDate()//日期
+  		let h=now.getHours()//小時
+  		let m=now.getMinutes()//分鐘
+  		let s=now.getSeconds()//秒數
+
+  		this.dateTime = y+'年'+M+'月'+d+'日'+h+'時'+m+'分'+s+'秒'
+  		setTimeout(this.showDateTime, 1000) //每間隔一秒就呼叫自已一次
+  	}
+  }
+}
+</script>
