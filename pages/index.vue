9821bd0c40eb1799b45a29377447c7d98e5fe240
quni <quniwork@gmail.com>
2019-11-26 15:07:41 +0800

    quni:20191126v1

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,32 @@
 <template lang='pug'>
-.login-wrap
-	.login-nav
-		a(href="#") 遠端協助
-		a(href="#") html5
-		a(href="#") 網頁版登入
-		a(href="#") 意見留言
-	.login-box
-		.login__header 登入
-		.login__form
-			el-form(label-width='40px')
-				el-form-item(label='線路')
-					el-select(placeholder='伺服器' v-model='server' style='width: 100%;')
-						el-option(label='1-伺服器' value='server1')
-				el-form-item(label='帳號')
-					el-input(v-model='account' placeholder='請输入帳號')
-				el-form-item(label='密碼')
-					el-input(v-model='password' type='password' placeholder='請输入密碼')
-				el-form-item
-					el-checkbox(v-model="rememberMe") 記住我
-				el-form-item
-					el-button(type='primary' native-type="submit" @click.native.prevent="doLogin") 登入
+.index
+	.backsite 新元氣理財【客戶端】備用站點：
+		a(href="#") http://xxx.xxx.cc
+		a(href="#") 加入最愛
+	.index-wrap
+		button.button-login(@click="loginShow = !loginShow") 網頁版登入
+	.login-wrap(v-if="loginShow == true")
+		.login-wrap_inner
+			.login-nav
+				a(href="#") 遠端協助
+				a(href="#") html5
+				a(href="#") 網頁版登入
+				a(href="#") 意見留言
+			.login-box
+				.login__header 登入
+				.login__form
+					el-form(label-width='40px')
+						el-form-item(label='線路')
+							el-select(placeholder='伺服器' v-model='server' style='width: 100%;')
+								el-option(label='1-伺服器' value='server1')
+						el-form-item(label='帳號')
+							el-input(v-model='account' placeholder='請输入帳號')
+						el-form-item(label='密碼')
+							el-input(v-model='password' type='password' placeholder='請输入密碼')
+						el-form-item
+							el-checkbox(v-model="rememberMe") 記住我
+						el-form-item
+							el-button(type='primary' native-type="submit" @click.native.prevent="doLogin") 登入
 	.loading(v-loading='loading' v-if="loading")
 </template>
 
@@ -40,6 +47,7 @@ export default {
 	data () {
 	  return {
 			loading: true,
+			loginShow: false,
 			account: '',
 			password: '',
 			rememberMe: '',
