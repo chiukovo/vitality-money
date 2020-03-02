51ed6ade4b8473448b42f0a7fd63a33655e48d07
Your Name <you@example.com>
2020-03-02 23:32:53 +0800

    add meta

--- pages/demo.vue
+++ pages/demo.vue
@@ -501,6 +501,10 @@ export default {
 	  	htmlAttrs: {
 	  	   class: 'wide wow-animation'
 	  	},
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
+      ],
 	    script: [
 	      { src: '/index/js/script.js', body: true },
 	    ],
